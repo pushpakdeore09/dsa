082535d590b43e790cdb716b74579d99b5b85355
pushpakdeore09 <pushpakdeore9114@gmail.com>
2025-09-29 23:43:22 +0530

    recursion

---
+++ src/Recursion/Main.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Recursion recursion = new Recursion();
+        //String s = "madam";
+        //System.out.println(recursion.checkPalindrome(0, s, s.length()));
+        //recursion.printOneTOn(10);
+        recursion.printNTOOne(10);
+    }
+}
--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -1,16 +1,28 @@
 package Recursion;
 
 public class Recursion {
-    public static void main(String[] args) {
-        String s = "madam";
-        System.out.println(checkPalindrome(0, s, s.length()));
-    }
 
-    public static boolean checkPalindrome(int i, String s, int n){
+
+    public boolean checkPalindrome(int i, String s, int n){
         if(i >= n/2){
             return true;
         }
         if(s.charAt(i) != s.charAt(n-i-1)) return false;
         return checkPalindrome(i+1, s, n);
+    }
+
+    public void printOneTOn(int n){
+        if (n > 0){
+            printOneTOn(n-1);
+            System.out.println(n);
+        }
+    }
+
+    public void printNTOOne(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        printNTOOne(n-1);
     }
 }
